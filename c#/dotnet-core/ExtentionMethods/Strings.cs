cc838f250258eca2df14fab4b52f53a0355fc2cb
Zee <zeesoft2005@gmail.com>
2018-10-13 15:20:01 +0500

    initiated

---
+++ c#/dotnet-core/ExtentionMethods/Strings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ExtentionMethods
+{
+    public static class Strings
+    {
+        /// <summary>
+        /// Truncates a string
+        /// </summary>
+        /// <param name="value">string input</param>
+        /// <param name="maxChars">limit of max characters to show as string</param>
+        /// <param name="trailingChar">character used to show missing/trucated characters; default: '.'</param>
+        /// <param name="repeatCount">no of times the trailingCharacter is to be shown; default: 3</param>
+        /// <returns></returns>
+        public static string Truncate(this string value, int maxChars, char trailingChar = '.', int repeatCount = 3)
+        {
+            if (string.IsNullOrEmpty(value))
+                return "";
+            var trailingStr = new string(trailingChar, repeatCount);
+            return value.Length <= maxChars ? value : value.Substring(0, maxChars) + trailingStr;
+        }
+    }
+}
